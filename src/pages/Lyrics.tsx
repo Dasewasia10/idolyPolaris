1b5649aae7ccfb3c0b3c4957f973797909196ae6
Dasewasia <daniel.stonford10@gmail.com>
2025-05-03 13:36:58 +0800

    modify lyrics page UI

--- src/pages/Lyrics.tsx
+++ src/pages/Lyrics.tsx
@@ -461,6 +461,16 @@ const Lyrics: React.FC = () => {
                       </div>
                     </div>
                   </div>
+                  <div className="hidden md:block lg:hidden xl:hidden 2xl:hidden">
+                    <VideoModal
+                      src={activeData[0]?.video}
+                      thumbnail={activeData[0]?.videoThumbnail}
+                      isOpen={videoModalIsOpen}
+                      setIsOpen={setVideoModalIsOpen}
+                      isSmall={videoModalIsSmall}
+                      setIsSmall={setVideoModalIsSmall}
+                    />
+                  </div>
                 </div>
               )}
 
@@ -481,6 +491,16 @@ const Lyrics: React.FC = () => {
                       <span className="text-gray-500">Tidak tersedia</span>
                     )}
                   </p>
+                  <div className="hidden md:block lg:hidden xl:hidden 2xl:hidden">
+                    <VideoModal
+                      src={activeData[0]?.video}
+                      thumbnail={activeData[0]?.videoThumbnail}
+                      isOpen={videoModalIsOpen}
+                      setIsOpen={setVideoModalIsOpen}
+                      isSmall={videoModalIsSmall}
+                      setIsSmall={setVideoModalIsSmall}
+                    />
+                  </div>
                 </div>
               )}
             </div>
