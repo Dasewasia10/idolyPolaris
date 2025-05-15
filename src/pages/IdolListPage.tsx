bfaff92a42924588919bfeaef09df2d99b344129
Dasewasia <daniel.stonford10@gmail.com>
2025-05-15 15:25:09 +0800

    disable all route, then rework the whole website and adding characters page

--- src/pages/IdolListPage.tsx
+++ src/pages/IdolListPage.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { motion, AnimatePresence } from "framer-motion";
-import QnAModal from "../components/QnaModal";
+import QnAModal from "../components/QnAModal";
 import { Character } from "../interfaces/Character";
 import {
   getGroupImageUrl,
